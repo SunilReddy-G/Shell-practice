872523c27d7f96779bfed976fdd02ee1f848eb34
sunilreddy.g <sunilsmec@gmail.com>
2026-01-29 21:46:59 +0530

    changed something

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# every program should present this command
+
+PERSON1=$1 #no space between value and
+PERSON2=$2
+
+echo "$PERSON1:: Hello $PERSON2, How are you?"
+echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking, How are you doing?"
+echo "$PERSON1:: Doing fine. What are you learning $PERSON2?"
+echo "$PERSON2:: I am learning shell. What about you?"
+
