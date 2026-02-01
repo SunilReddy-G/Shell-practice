300e4b824f2d1c8d0065354b5bb81e333533ea41
sunilreddy.G <sunilsmec@gmail.com>
2026-02-01 13:18:56 +0530

    change something

--- 08-variables.sh
+++ 08-variables.sh
@@ -1,3 +1,8 @@
 #!bin/b
-TIMESTAMP=$(date)
+START_TIME=$(date +%s)
 echo "script executed at: $TIMESTAMP"
+sleep 10
+
+END_TIME=$(date +%s)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
+echo " Script excuted at: $TOTAL_TIME"
