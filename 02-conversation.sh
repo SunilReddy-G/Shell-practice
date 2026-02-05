92b35e4a4b5470a781ac36e797c536f8e4ad0a98
sunilreddy.g <sunilsmec@gmail.com>
2026-02-05 20:54:49 +0530

    changed something

--- 02-conversation.sh
+++ 02-conversation.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # every program should present this command
+
 echo "Suresh:: Hello Ramesh, How are you?"
 echo "Ramesh:: Hi suresh, I am fine thanks for asking, How are you doing?"
 echo "Suresh:: Doing fine. What are you learning Ramesh?"
--- 03-variables.sh
+++ 03-variables.sh
@@ -8,3 +8,4 @@ echo "$PERSON1:: Hello $PERSON2, How are you?"
 echo "$PERSON2:: Hi $PERSON1, I am fine thanks for asking, How are you doing?"
 echo "$PERSON1:: Doing fine. What are you learning $PERSON2?"
 echo "$PERSON2:: I am learning shell. What about you?"
+
