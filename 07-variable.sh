76e062e30e850bb88729e61cc0854b2b5f34c893
sunilreddy.g <sunilsmec@gmail.com>
2026-02-07 21:26:14 +0530

    practicing

---
+++ 07-variable.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+##### Special variables
+echo "All args passed to the script: $@"
+echo "Number of var passed the script: $#"
+echo " Script name: $0"
+echo " who is running: $USER"
+echo "Home directory of the current user $HOME"
+echo "PID of the script: $$"
+sleep 100 &
+echo "PID of recently executed background process $!"
+echo "All args passed to the script $*"
