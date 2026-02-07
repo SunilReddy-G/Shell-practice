9c9f5596e0bd235adc74bb57390de7b2da02b128
sunilreddy.g <sunilsmec@gmail.com>
2026-02-07 21:27:05 +0530

    practicing

--- 07-variable.sh
+++ 07-variable.sh
@@ -8,4 +8,4 @@ echo "Home directory of the current user $HOME"
 echo "PID of the script: $$"
 sleep 100 &
 echo "PID of recently executed background process $!"
-echo "All args passed to the script $*"
+echo "All args passed to the script: $*"
